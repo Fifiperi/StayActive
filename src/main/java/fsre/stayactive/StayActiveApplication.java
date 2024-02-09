87360c68711650f69f2da897614553378e4e105e
Filip <fifi.peri3@gmail.com>
2024-02-09 15:09:47 +0100

    test

---
+++ src/main/java/fsre/stayactive/StayActiveApplication.java
@@ -0,0 +1,13 @@
+package fsre.stayactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayActiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayActiveApplication.class, args);
+    }
+
+}
