fcc136ed5c149143130204dc921b4eee9c05ff05
Filip <fifi.peri3@gmail.com>
2024-02-11 15:56:08 +0100

    Login in registar rade

--- src/main/java/fsre/stayactive/controllers/WorkoutController.java
+++ src/main/java/fsre/stayactive/controllers/WorkoutController.java
@@ -1,4 +1,13 @@
 package fsre.stayactive.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WorkoutController {
+
+    @GetMapping("/user/gender")
+    public String showGender(){
+        return "user/gender";
+    }
 }
